f0aa1ef8d1f249fd6cef5dfb2c057da2e76a41aa
henrique.castro <henrique.castro@pattrol.com.br>
2022-06-21 18:36:36 -0300

    adds maps

--- src/DataVizualizer.jsx
+++ src/DataVizualizer.jsx
@@ -10,8 +10,14 @@ import {
   Title,
 } from "chart.js";
 import { Doughnut, Bar } from "react-chartjs-2";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+} from "react-simple-maps";
 
-import { getTotalVotes } from "./data/datamanager";
+import { getTotalVotes, getGeoMarkers } from "./data/datamanager";
 import { pieData, barData } from "./data/graphManager";
 
 ChartJS.register(
@@ -36,14 +42,66 @@ const options = {
   },
 };
 
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
 const DataVizualizer = () => {
+  const [markers, setMarkers] = React.useState([]);
+
+  React.useEffect(()=>{
+    setMarkers(getGeoMarkers())
+  },[]);
   return (
     <>
       <h1>Votos Totais</h1>
+      <ComposableMap
+        projection="geoAzimuthalEqualArea"
+        projectionConfig={{
+          rotate: [58, 20, 0],
+          scale: 400,
+        }}
+      >
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies
+              .filter((d) => d.properties.REGION_UN === "Americas")
+              .map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill="#EAEAEC"
+                  stroke="#D6D6DA"
+                />
+              ))
+          }
+        </Geographies>
+        {markers.map(({ name, coordinates, markerOffset }) => (
+          <Marker key={name} coordinates={coordinates}>
+            <g
+              fill="none"
+              stroke="#FF5533"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              transform="translate(-12, -24)"
+            >
+              <circle cx="12" cy="10" r="3" />
+              <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+            </g>
+            <text
+              textAnchor="middle"
+              y={markerOffset}
+              style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+            >
+              {name}
+            </text>
+          </Marker>
+        ))}
+      </ComposableMap>
       <div>
         <Doughnut data={pieData(getTotalVotes())} />
       </div>
-      <div style={{height:50}}>
+      <div style={{ height: 50, marginBottom: 80 }}>
         <Bar options={options} data={barData(getTotalVotes())} />
       </div>
     </>
--- src/data/datamanager.js
+++ src/data/datamanager.js
@@ -36,21 +36,35 @@ export function getTotalVotes() {
   return users;
 }
 
+export function getGeoMarkers() {
+  const users = JSON.parse(localStorage.getItem("votes"));
+  const markers = users.map((e) => {
+    return {
+      markerOffset: 15,
+      name: e.vote,
+      coordinates: [e.longitude, e.latitude],
+    };
+  });
+  return markers;
+}
+
 export function createDB() {
   let users = localStorage.getItem("votes");
 
   if (!users) {
     const voteOptions = ["lula", "bolsonaro", "outro", "nulo", "branco"];
-    const genderOptions = ['M', 'F', 'O'];
+    const genderOptions = ["M", "F", "O"];
     const data = [];
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 20; i++) {
+      let longOff = Math.random() < 0.5 ? -1 : 1;
+      let latOff = Math.random() < 0.5 ? -1 : 1;
       data.push({
         nome: "",
         gender: genderOptions[Math.floor(Math.random() * genderOptions.length)],
         vote: voteOptions[Math.floor(Math.random() * voteOptions.length)],
         age: "jovem",
-        longitude: "-43.9516541",
-        latitude: "-19.9528853",
+        longitude: -47.8919 + Math.random() * 10 * longOff,
+        latitude: -15.7975 + Math.random() * 5 * latOff,
       });
     }
     localStorage.setItem("votes", JSON.stringify(data));
